8eb3e1cdf51860687be6991c55225aca6859811e
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-04-17 13:12:01 -0500

    export table currency

--- projects/gs-tables/src/public-api.ts
+++ projects/gs-tables/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/gs-tables.service';
 export * from './lib/gs-tables.component';
 export * from './lib/gs-tables.module';
 export * from './lib/gs-tables.widgets';
+export * from './lib/gs-tables.enum';
+export * from './lib/gs-tables.constants';
